5a5c90299bca9800d6dd58a72da7f8d09519fd18
Winebald <w.banituze@alustudent.com>
2025-07-22 09:38:32 -0700

    added shell script

---
+++ analyze_data.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Clean the repository
+mv rename_directory analyzed_data
+rm -r dummy raw_data/dummy-2 analyzed_data/dummy-3.txt
+mv satelite_temperature_data.csv raw_data/
+
+# Analyze the data
+cd raw_data
+
+# Extract top 10 highest temperatures
+head -n 1 satelite_temperature_data.csv | cut -d, -f1-3 > ../analyzed_data/highest_temp.csv
+sort -t, -k3 -nr satelite_temperature_data.csv | head -n 10 | cut -d, -f1-3 >> ../analyzed_data/highest_temp.csv
+
+# Extract humidity data for Mauritius
+head -n 1 satelite_temperature_data.csv | cut -d, -f1,2,4 > ../analyzed_data/humidity_data_Mauritius.csv
+grep "Mauritius" satelite_temperature_data.csv | sort -t, -k4 -nr | cut -d, -f1,2,4 >> ../analyzed_data/humidity_data_Mauritius.csv
+
+echo "Data analysis completed successfully!"
